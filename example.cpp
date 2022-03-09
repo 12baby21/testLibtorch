767c5a3221d120fb645b42e9d133ea29083d5165
12baby21 <18917131713@163.com>
2022-03-09 17:41:42 +0800

    2

--- example.cpp
+++ example.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 using namespace torch::autograd;
 #include <fstream>
+#include <cmath>
 
 // Define a new Module.
 class Net : public torch::nn::Module
@@ -57,25 +58,46 @@ public:
 
   static torch::Tensor calGrad_weight(torch::Tensor pred, torch::Tensor label, torch::Tensor input)
   {
-    auto gradWeight = (label - pred).t() * input.reshape({input.size(0), 784});
+    auto newLabel = label.item<int>();
+    torch::Tensor correct = torch::zeros({1, 2});
+    if (newLabel == 1)
+    {
+      correct[0][1] = 1;
+    }
+    else
+    {
+      correct[0][0] = 1;
+    }
+
+    auto gradWeight = -1 * torch::mm((correct - pred).t(), input.reshape({input.size(0), 784}));
 
     return gradWeight;
   }
 
   static torch::Tensor calGrad_bias(torch::Tensor pred, torch::Tensor label)
   {
-    auto gradBias = (label - pred).squeeze_();
+    auto newLabel = label.item<int>();
+    torch::Tensor correct = torch::zeros({1, 2});
+    if (newLabel == 1)
+    {
+      correct[0][1] = 1;
+    }
+    else
+    {
+      correct[0][0] = 1;
+    }
+    auto gradBias = -1 * (correct - pred).squeeze();
     return gradBias;
   }
 
-  static void SGD_UpdateWeight(torch::Tensor gradWeight, torch::Tensor &weight, int learning_rate = 0.1)
+  static void SGD_UpdateWeight(torch::Tensor &gradWeight, torch::Tensor &weight, int learning_rate = 0.1)
   {
-    weight = weight - learning_rate * gradWeight / 60000;
+    weight = weight - learning_rate * gradWeight;
   }
 
   static void SGD_UpdateBias(torch::Tensor gradBias, torch::Tensor &bias, int learning_rate = 0.1)
   {
-    bias = bias - learning_rate * gradBias / 60000;
+    bias = bias - learning_rate * gradBias;
   }
 };
 
@@ -83,24 +105,22 @@ int main()
 {
   // Create Nets of PartyA and PartyB.
   auto PartyA = std::make_shared<Net>();
-  auto PartyB = std::make_shared<Net>();
+  // auto PartyB = std::make_shared<Net>();
 
   // Create data loader.
   auto data_loader = torch::data::make_data_loader(
       torch::data::datasets::MNIST("/home/wjf/Desktop/testLibtorch/data").map(torch::data::transforms::Stack<>()));
-  auto test_loader = torch::data::make_data_loader(
-      torch::data::datasets::MNIST("/home/wjf/Desktop/testLibtorch/data", torch::data::datasets::MNIST::Mode::kTest).map(torch::data::transforms::Stack<>()));
 
   std::ofstream sfile("./log.txt", ios::out);
   int lr = 0.1;
-  for (size_t epoch = 1; epoch <= 3; ++epoch)
+  for (size_t epoch = 1; epoch <= 5; ++epoch)
   {
+    float lossSum = 0;
     size_t batch_index = 0;
     // Iterate the data loader to yield batches from the dataset.
     for (auto &batch : *data_loader)
     {
-      torch::Tensor tmp = torch::zeros_like(batch.target);
-      if (torch::equal(batch.target % 2, tmp))
+      if (batch.target.item<int>() % 2, 0)
       {
         batch.target.fill_(0);
       }
@@ -111,32 +131,46 @@ int main()
 
       // Execute the model on the input data.
       torch::Tensor predictionA = PartyA->forward(batch.data);
-      torch::Tensor predictionB = PartyB->forward(batch.data);
-      
-      // partyA的预测值+partyB的预测值
-      torch::Tensor newPrediction = 0.5 * predictionA + 0.5 * predictionB;
+      // torch::Tensor predictionB = PartyB->forward(batch.data);
 
-      delete &predictionA;
+      // partyA的预测值+partyB的预测值
+      // torch::Tensor newPrediction = 0.5 * predictionA + 0.5 * predictionB;
 
       // 计算loss
+      /*
+      auto index = (torch::argmax(predictionA)).item<int>();
+      float _y = predictionA[0][index].item<float>();
+      auto y = (batch.target).item<int>();
+      cout << _y << endl;
+      cout << y << endl;
+      auto loss = y * log2f32(_y) + (1 - y) * log2f32(1 - _y);
+      lossSum -= loss;
+      */
       auto loss = torch::cross_entropy_loss(predictionA, batch.target);
+      // auto loss = torch::cross_entropy_loss(newPrediction, batch.target);
+      lossSum += loss.item<float>();
 
       // 更新模型
+      auto gradWeight = myBackward::calGrad_weight(predictionA, batch.target, batch.data);
+      auto gradBias = myBackward::calGrad_bias(predictionA, batch.target);
+      myBackward::SGD_UpdateWeight(gradWeight, PartyA->fc1->weight, lr);
+      myBackward::SGD_UpdateBias(gradBias, PartyA->fc1->bias, lr);
+      /*
       auto gradWeight = myBackward::calGrad_weight(newPrediction, batch.target, batch.data);
       auto gradBias = myBackward::calGrad_bias(newPrediction, batch.target);
       myBackward::SGD_UpdateWeight(gradWeight, PartyA->fc1->weight, lr);
       myBackward::SGD_UpdateBias(gradBias, PartyA->fc1->bias, lr);
       myBackward::SGD_UpdateWeight(gradWeight, PartyB->fc1->weight, lr);
       myBackward::SGD_UpdateBias(gradBias, PartyB->fc1->bias, lr);
+      */
 
+      ++batch_index;
       // Output the loss and checkpoint every 100 batches.
-      if (++batch_index % 10000 == 0)
+      if (batch_index % 10000 == 0)
       {
         cout << "We have trained " << epoch << " epochs..." << endl;
         sfile << "Epoch: " << epoch << " | Batch: " << batch_index
-              << " | Loss: " << loss.item<float>() << std::endl;
-
-        
+              << " | Average Loss: " << lossSum / batch_index << std::endl;
       }
     }
   }
--- some codes.cpp
+++ some codes.cpp
@@ -1,26 +1,31 @@
+  auto test_loader = torch::data::make_data_loader(
+      torch::data::datasets::MNIST("/home/wjf/Desktop/testLibtorch/data", torch::data::datasets::MNIST::Mode::kTest).map(torch::data::transforms::Stack<>()));
+
+
+
 int correct = 0;
-        for (auto &testBatch : *test_loader)
-        {
-          if (torch::equal(testBatch.target % 2, tmp))
-          {
-            testBatch.target.fill_(0);
-          }
-          else
-          {
-            testBatch.target.fill_(1);
-          }
+for (auto &testBatch : *test_loader)
+{
+    if (torch::equal(testBatch.target % 2, tmp))
+    {
+        testBatch.target.fill_(0);
+    }
+    else
+    {
+        testBatch.target.fill_(1);
+    }
 
-          auto pred = PartyA->forward(testBatch.data);
+    auto pred = PartyA->forward(testBatch.data);
 
-          pred.squeeze_();
-          torch::Tensor ans = torch::argmax(pred);
+    pred.squeeze_();
+    torch::Tensor ans = torch::argmax(pred);
 
-          bool flag = torch::equal(ans, testBatch.target);
+    bool flag = torch::equal(ans, testBatch.target);
 
-          if (flag)
-          {
-            correct += 1;
-          }
-        }
-        cout << correct << endl;
-        sfile << "accuracy: " << (static_cast<double>(correct) / 10000.0) << endl;
+    if (flag)
+    {
+        correct += 1;
+    }
+}
+cout << correct << endl;
+sfile << "accuracy: " << (static_cast<double>(correct) / 10000.0) << endl;
